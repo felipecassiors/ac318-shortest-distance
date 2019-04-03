71c72745f43233160cd2f5eb6a5c8aa61f9df51f
Felipe Santos <felipecassiors@gmail.com>
2019-04-02 21:20:27 -0300

    Calculates the short distance and show

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const csv = require('csvtojson');
 const haversine = require('haversine');
+const FloydWarshall = require('floyd-warshall');
+require('console.table');
 
 csv({delimiter: ";"}).fromFile("./coordenadas.csv").then((input) => {
-    var cities = [];
-    input.forEach(i => {
+    let cities = [];
+    let names = [];
+    for (let i of input) {
         cities.push({
             name: i.Capital,
             latitude: i.Latitude,
             longitude: i.Longitude,
-            distances: []
         });
-    });
+        names.push(i.Capital);
+    }
 
-    cities.forEach(cityA => {
-        cities.forEach(cityB => {
-            cityA.distances.push({
-                to: cityB.name,
-                distance: haversine(cityA, cityB)
-            });
-        });
-    })
+    let distances = [];
+    for (let i = 0; i < cities.length; i++) {
+        distances[i] = [];
+        for (let j = 0; j < cities.length; j++) {
+            distances[i][j] = haversine(cities[i], cities[j]);
+        }
+    }
+
+    console.log("Matrix of the distances:")
+    console.table(names, distances);
+
+    const floydWarshall = new FloydWarshall(distances);
+    let shortestDistances = floydWarshall.shortestPaths;
+
+    console.log("Matrix of the shortest distances:")
+    console.table(names, shortestDistances);
 });
